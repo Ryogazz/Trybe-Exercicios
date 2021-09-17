7c4d0bb97de6a3e9ee8f05c36bb70c7fe91338aa
Ryogazz <josuex.x.x@hotmail.com>
2021-09-16 21:56:44 -0300

    exercicio 6.2 inicio

---
+++ Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -0,0 +1,40 @@
+function criarElementoOpstion() {
+    const estados = document.querySelector("#estate");//captura pelo ID 
+
+    const states = [
+        { value: 'AC', label: 'Acre' },
+        { value: 'AL', label: 'Alagoas' },
+        { value: 'AP', label: 'Amapá' },
+        { value: 'AM', label: 'Amazonas' },
+        { value: 'BA', label: 'Bahia' },
+        { value: 'CE', label: 'Ceará' },
+        { value: 'DF', label: 'Distrito Federal' },
+        { value: 'ES', label: 'Espírito Santo' },
+        { value: 'GO', label: 'Goías' },
+        { value: 'MA', label: 'Maranhão' },
+        { value: 'MT', label: 'Mato Grosso' },
+        { value: 'MS', label: 'Mato Grosso do Sul' },
+        { value: 'MG', label: 'Minas Gerais' },
+        { value: 'PA', label: 'Pará' },
+        { value: 'PB', label: 'Paraíba' },
+        { value: 'PR', label: 'Paraná' },
+        { value: 'PE', label: 'Pernambuco' },
+        { value: 'PI', label: 'Piauí' },
+        { value: 'RJ', label: 'Rio de Janeiro' },
+        { value: 'RN', label: 'Rio Grande do Norte' },
+        { value: 'RS', label: 'Rio Grande do Sul' },
+        { value: 'RO', label: 'Rondônia' },
+        { value: 'RR', label: 'Roraíma' },
+        { value: 'SC', label: 'Santa Catarina' },
+        { value: 'SP', label: 'São Paulo' },
+        { value: 'SE', label: 'Sergipe' },
+        { value: 'TO', label: 'Tocantins' },
+    ];
+    //gera de forma dinamica o option no HTML
+    for (let index = 0; index < states.length; index++) {
+        let criarElementoOpstion = document.createElement("option"); //cria option no html
+        estados.appendChild(criarElementoOpstion).innerText = states[index].value; // coloca dentro do option o value do state, e poe ele como filho do elemento criado "option";
+        estados.appendChild(criarElementoOpstion).value = states[index].label;//coloca em option o valor (label) do state e poe como filjo do option criado
+    }
+}
+criarElementoOpstion();
